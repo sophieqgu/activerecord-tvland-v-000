3e032774f74d031943d5dd2f624857007dbc3924
Sophie Gu <sophieqgu@gmail.com>
2019-06-26 23:51:06 +0000

    Automatically backed up by Learn

--- db/migrate/004_add_actors_name.rb
+++ db/migrate/004_add_actors_name.rb
@@ -1,1 +1,6 @@
-class AddActorsName 
+class AddActorsName < ActiveRecord::Migration[5.1]
+  def change
+    add_column :artists, :first_name, :string 
+    add_column :artists, :last_name, :string 
+  end 
+end 
