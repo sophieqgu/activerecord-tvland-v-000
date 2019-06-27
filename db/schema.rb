c6d52860df600428896ecaefb351769368e19e23
Sophie Gu <sophieqgu@gmail.com>
2019-06-27 00:29:37 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 6) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "show_id"
+    t.string "catchphrase"
+  end
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
