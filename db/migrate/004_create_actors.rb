64d1a8e75d06dd85dda6e232194002fe2939059c
Sophie Gu <sophieqgu@gmail.com>
2019-06-27 00:30:35 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,6 @@
-class CreateArtists < ActiveRecord::Migration[5.1]
+class CreateActors < ActiveRecord::Migration[5.1]
   def change
-    create_table :artists do |t|
+    create_table :actors do |t|
       t.string :first_name
       t.string :last_name 
     end 
