687158561641dc8a375f7cfa1efd49d579654858
Sophie Gu <sophieqgu@gmail.com>
2019-06-27 00:16:40 +0000

    Automatically backed up by Learn

--- db/migrate/add_characters_name_and_id.rb
+++ db/migrate/add_characters_name_and_id.rb
@@ -1,1 +1,6 @@
-class AddCharactersNameAndId < 
+class AddCharactersNameAndId < ActiveRecord::Migration[5.1]
+  def change 
+    add_column :characters, :name, :string 
+    add_column :characters, :show_id, :integer 
+  end 
+end 
