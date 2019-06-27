8931ce61d11e21cfb70a3eee37c91275d987b614
Sophie Gu <sophieqgu@gmail.com>
2019-06-27 00:51:09 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_characters_to_show.rb
+++ db/migrate/009_add_characters_to_show.rb
@@ -1,5 +1,5 @@
-class AddCharacterToShow < ActiveRecord::Migration[5.1]
+class AddCharactersToShow < ActiveRecord::Migration[5.1]
   def change
-    add_column :characters, :actor_id, :integer
+    add_column :characters, :show_id, :integer
   end
 end
