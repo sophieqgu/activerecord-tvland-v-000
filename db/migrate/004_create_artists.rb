ba2503d53e9076d393f74c7928b9ea2462340c67
Sophie Gu <sophieqgu@gmail.com>
2019-06-27 00:27:35 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_artists.rb
+++ db/migrate/004_create_artists.rb
@@ -1,8 +1,8 @@
 class CreateArtists < ActiveRecord::Migration[5.1]
   def change
     create_table :artists do |t|
-      t.string = :first_name
-      t.string = :last_name 
+      t.string :first_name
+      t.string :last_name 
     end 
   end 
 end 
